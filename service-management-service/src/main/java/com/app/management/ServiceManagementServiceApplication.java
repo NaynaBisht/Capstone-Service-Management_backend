531d34766c961636849594a00538f5518a281634
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2025-12-29 22:45:09 +0530

    service management microservice setup

---
+++ service-management-service/src/main/java/com/app/management/ServiceManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceManagementServiceApplication.class, args);
+	}
+
+}
---
+++ service-management-service/src/test/java/com/app/servicemgt/ServiceManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.servicemgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
