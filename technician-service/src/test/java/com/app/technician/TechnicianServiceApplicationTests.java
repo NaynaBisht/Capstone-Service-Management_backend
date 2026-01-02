8569bef9995b1943007aef58cb81fb16382be62c
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2026-01-02 10:09:51 +0530

    technician service setup

---
+++ technician-service/src/test/java/com/app/technician/TechnicianServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.technician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicianServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
