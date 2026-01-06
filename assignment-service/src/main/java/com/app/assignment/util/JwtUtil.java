c9f824130deb09a2041929bfaea6469a00679159
NaynaBisht <nayna.22bce8037@vitapstudent.ac.in>
2026-01-07 01:28:03 +0530

    Removed sensitive data

--- assignment-service/src/main/java/com/app/assignment/util/JwtUtil.java
+++ assignment-service/src/main/java/com/app/assignment/util/JwtUtil.java
@@ -3,6 +3,8 @@ package com.app.assignment.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -10,10 +12,11 @@ import java.security.Key;
 @Component
 public class JwtUtil {
 
-    private static final String SECRET = "YourSuperLongAndSecureSecretKeyThatIsAtLeast32Chars!";
+    @Value("${jwt.secret}")
+    private String secret;
 
     private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public Claims extractAllClaims(String token) {
